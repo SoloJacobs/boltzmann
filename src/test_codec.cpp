e67c1a3c0e15f2ac509841e00f762c40512a3ee3
UbuntuAws <amazon.honeymooners@aleeas.com>
2021-10-26 15:40:15 +0000

    removed unused namespace by commenting it out

--- src/test_codec.cpp
+++ src/test_codec.cpp
@@ -3,15 +3,15 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 
-namespace fs = std::filesystem;
+// namespace fs = std::filesystem;
 
 int main() {
   cv::VideoWriter output;
 
   std::string path{"/home/sol/cpp_basics/boltzmann/build/test_image.png"};
-//cv::Mat inImg = cv::imread(path, cv::IMREAD_GRAYSCALE);
- // auto size = inImg.size();
-  cv::Mat inImg(cv::Size(100,100),CV_8UC1);
+  // cv::Mat inImg = cv::imread(path, cv::IMREAD_GRAYSCALE);
+  // auto size = inImg.size();
+  cv::Mat inImg(cv::Size(100, 100), CV_8UC1);
   output.open("live1.mp4", cv::VideoWriter::fourcc('a', 'v', 'c', '1'), 15.0,
               inImg.size(), false);
   for (int i{0}; i < 100; ++i) {
@@ -20,10 +20,10 @@ int main() {
   size_t i{0};
   for (auto it = inImg.begin<unsigned char>(); it != inImg.end<unsigned char>();
        ++it) {
-	  ++i;
+    ++i;
     *it = i;
   }
   for (int i{0}; i < 100; ++i) {
-	  output << inImg;
+    output << inImg;
   }
 }
