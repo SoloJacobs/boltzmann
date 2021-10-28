df8b2b1f85f503684bfd7e84c2557b8e9cc0a2ce
UbuntuAws <amazon.honeymooners@aleeas.com>
2021-10-28 17:00:40 +0000

    add build scripts

---
+++ clean_build.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+TOP_DIR=$(git rev-parse --show-toplevel)
+CUR_DIR=$(pwd)
+cd $TOP_DIR/build/
+
+rm $TOP_DIR/build -rf
+mkdir $TOP_DIR/build
+cd $TOP_DIR/build
+cmake .. -G Ninja -DCMAKE_CXX_COMPILER=clang++
+cmake --build .
+cd $CUR_DIR
---
+++ rebuild.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+TOP_DIR=$(git rev-parse --show-toplevel)
+CUR_DIR=$(pwd)
+cd $TOP_DIR/build/
+cmake .. -G Ninja -DCMAKE_CXX_COMPILER=clang++
+cmake --build .
+cd $CUR_DIR
