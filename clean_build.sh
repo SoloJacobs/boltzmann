1e4b808052903b89c3e53d9dce5f26c811f4f201
UbuntuAws <amazon.honeymooners@aleeas.com>
2021-11-04 16:31:25 +0000

    push cleanup changes before merge

--- clean_build.sh
+++ clean_build.sh
@@ -4,8 +4,7 @@ TOP_DIR=$(git rev-parse --show-toplevel)
 CUR_DIR=$(pwd)
 cd $TOP_DIR/build/
 
-rm $TOP_DIR/build -rf
-mkdir $TOP_DIR/build
+rm $TOP_DIR/build/* -rf
 cd $TOP_DIR/build
 cmake .. -G Ninja -DCMAKE_CXX_COMPILER=clang++
 cmake --build .
